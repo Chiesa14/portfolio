65bade5ce4fce208dfac96dd2f163263fb260bbd
Chiesa14 <remychiesa14@gmail.com>
2025-07-27 10:18:52 +0200

    build fixes

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-import { loadConfig } from 'next/dist/server/config';
-import { register } from 'esbuild-register/dist/node';
-
-register();
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-export default require('./next.config.ts').default;
